9475c4979caa469068b4050c4c9852bd20bd0856
JianQiangYang <itame@XX.com>
2019-12-18 19:50:06 +0800

    新增文件

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 
 import ElementUI from 'element-ui' // 引入包
 import 'element-ui/lib/theme-chalk/index.css'// 引入elementUI样式
-
 import './styles/index.less'// 引入初始化样式(老师项目中 拷贝的)
 
 Vue.config.productionTip = false
