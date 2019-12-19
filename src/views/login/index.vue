c3ef91aa71a31ae53adb20710290c850fb234640
JianQiangYang <itame@XX.com>
2019-12-19 16:12:00 +0800

    登录页面布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,29 @@
 <template>
 <div class="login">
-<el-card class="card"></el-card>
+    <!-- 放置卡片组件 -->
+<el-card class="card">
+    <div class="title">
+        <img src="../../assets/img/logo_index.png" alt="">
+    </div>
+    <!-- 登录 表单  表单容器 -->
+    <el-form style="margin-top:30px">
+        <el-form-item>
+           <el-input placeHolder='请输入手机号'></el-input>
+        </el-form-item>
+        <el-form-item>
+           <el-input placeHolder='请输入验证码' style="width:65%"></el-input>
+           <el-button  style="float:right" plain>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+            <el-checkbox>
+                我已阅读并同意<a href="#">用户协议和隐私条款</a>
+            </el-checkbox>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+    </el-form>
+</el-card>
 </div>
 </template>
 
@@ -15,9 +38,18 @@ export default {
     background-image: url(../../assets/img/login_bg.jpg);
     background-size: cover;// cover:把背景图像扩展至足够大，以使背景图像完全覆盖背景区域。背景图像的某些部分也许无法显示在背景定位区域中。
     height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     .card{
         width: 440px;
         height:350px;
+        .title{
+            text-align: center;
+            img{
+                height: 50px;
+            }
+        }
     }
 }
 </style>
