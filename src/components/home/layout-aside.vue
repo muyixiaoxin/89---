e31b73a08f45bb37e30770223bd372578e9dd779
JianQiangYang <itame@XX.com>
2019-12-21 01:03:41 +0800

    home界面大纲拼接完成

---
+++ src/components/home/layout-aside.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- <div>左侧导航</div> -->
+  <el-menu>
+      <el-menu-item>首页</el-menu-item>
+      <el-submenu>
+          <template slot="title">
+              <span>内容管理</span>
+          </template>
+          <el-menu-item-group>
+          <el-menu-item>发布文章</el-menu-item>
+          <el-menu-item>内容列表</el-menu-item>
+          <el-menu-item>评论列表 </el-menu-item>
+          <el-menu-item>素材管理</el-menu-item>
+          </el-menu-item-group>
+      </el-submenu>
+      <el-submenu>
+          <template slot="title">
+              <span>粉丝管理</span>
+          </template>
+          <el-menu-item-group>
+          <el-menu-item>粉丝概况</el-menu-item>
+          <el-menu-item>图文数据</el-menu-item>
+          <el-menu-item>粉丝概况</el-menu-item>
+          <el-menu-item>粉丝列表</el-menu-item>
+          </el-menu-item-group>
+      </el-submenu>
+    <el-menu-item>账户信息</el-menu-item>
+
+  </el-menu>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="one">
-   <span>主页</span>
-    </div>
+<!-- 外层容器 -->
+<el-container>
+<!-- 左右容器 -->
+<el-aside>
+  <layout-aside></layout-aside>
+</el-aside>
+<el-container>
+  <!-- 上下布局 -->
+  <el-header>右侧 头部 </el-header>
+  <el-main>右侧 主要区域</el-main>
+</el-container>
+</el-container>
 </template>
 
 <script>
+import layoutAside from '../../components/home/layout-aside'
 export default {
-
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
